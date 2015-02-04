8e8979d125af53f0f2c96b8610c9fecb76f37a44
HannaKorn <hanna.korn@web.de>
2015-02-04 12:32:35 +0100

    Datei hinzugefügt

---
+++ Mehrwertsteuer.rb
@@ -0,0 +1,12 @@
+# Variablendefinition
+betrag = 250
+
+# Berechnung der Mehrwertsteuer
+mwst = betrag * 0.19
+
+# Anzeige der Ergebnisse auf der Konsole 
+puts "Mehrwertsteuerberechnung:"
+puts "Betrag: "
+puts betrag
+puts "Mehrwertsteuer: "
+puts mwst
